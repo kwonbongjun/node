39b136ef352108ba6bc959c1696114b96b6580ed
bong <bongjun777@naver.com>
2019-07-05 17:13:37 +0900

    20190705

--- app/config.js
+++ app/config.js
@@ -1,17 +1,17 @@
 module.exports = {
     server_port: 80,
     server_session: {
-        secret:"keyboard cat",
-        resave:false,
-        saveUninitialized:true
+        secret:"keyboard cat",//비밀키. 쿠키 임의 변경 방지
+        resave:false,//변경되지 않아도 언제나 저장할지
+        saveUninitialized:true //세션 저장 전에 초기화안한 상태로 미리 만들어 저장
     },
     server_error: {
       static: {
-        "404":"./error/404.html"
+        "404":"./error/404.html" //정적폴더
       }
     },
     jdbc : {
-      connectionLimit:5,
+      connectionLimit:1,
 
   /*    host:"192.168.3.31",
       user:"root",
