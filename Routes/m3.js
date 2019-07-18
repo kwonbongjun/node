84d4bc5be2bcbd6d6eba17aa1636b61a8c3ae54c
bong <bongjun777@naver.com>
2019-07-18 18:25:01 +0900

    20190718

--- Routes/m3.js
+++ Routes/m3.js
@@ -18,7 +18,7 @@ m[1] = {                        // 모듈 리스트에 넣기
     },
     step1: function(req, res){
         var index = Number(req.body.index);
-        var sql = `select * from a_budgets LIMIT ?, 5`;
+        var sql = `select budgets_id,DATE_FORMAT(date,'%Y-%m-%d') as date ,division,money from a_budgets order by budgets_id desc LIMIT ?, 5`;
         var result = {state : true};
         db("GET", sql, [index]).then(function(rows){
             if(rows.state){
@@ -116,19 +116,21 @@ m[6] = {
     path: "/update",
     type: "POST",
     fun: function(req, res){
-        var id = req.session.user.id;
-        var paramId = req.body.id;
-        var no = req.body.no;
-        var nm = req.body.nm;
-        if(id == paramId){
-            var sql = "update test set NM = ? where NO = ?";
-            db("SET", sql, [nm, no]).then(function(rows){
+        console.log(req.body);
+        //var id = req.session.user.id;
+        var b_id = req.body.budgets_id;
+        var date = req.body.date;
+        var division = req.body.division;
+        var money = req.body.money;
+        //if(id == paramId){
+            var sql = "update test set date = ?,division = ?, money= ? where budgets_id = ?";
+            db("SET", sql, [date,division,money,b_id]).then(function(rows){
                 commons.msgResult(res, rows, "msg2", "");
             });
-        } else {
-            var rows = {state : false};
-            commons.msgResult(res, rows, "msg2", "");
-        }
+        //} else {
+          //  var rows = {state : false};
+          //  commons.msgResult(res, rows, "msg2", "");
+        //}
     }
 }
 m[7] = {                        // 모듈 리스트에 넣기
@@ -138,6 +140,69 @@ m[7] = {                        // 모듈 리스트에 넣기
         commons.viewResult(req, res, "./m3/index2.html");
     }
 }
+m[8] = {
+    path: "/createview",
+    type: "POST",
+    fun: function(req, res){
+
+        var sql = `CREATE OR REPLACE VIEW v_a_money as 
+        SELECT type_flag AS 구분, b.부서, publisheddate AS  발생일자, price AS  금액,  reciver  AS  대상,ids   AS   항목
+        FROM b_voucher cross join (SELECT '영업' AS 부서) b
+        UNION 
+        SELECT b.구분, b.부서, pay_date AS  발생일자, total_salary AS  금액, b.항목, b.대상
+        FROM p_pay_dailies cross join (SELECT '인사' AS 부서, '출금' AS '구분', '부서내' AS  대상,'일용직' AS  항목 ) b
+        UNION
+        SELECT b.구분, b.부서, pay_date AS  발생일자, total_salary AS  금액, b.항목, b.대상
+        FROM p_pay_permanents cross join (SELECT '인사' AS 부서, '출금' AS '구분', '부서내' AS  대상,'상용직' AS  항목 ) b`;
+        
+        db("GET", sql, []).then(function(rows){
+           // commons.msgResult(res, rows, "msg1", "../m3/index1.html#c1");//이동주소
+        });
+        m[8].step1(req,res);
+    },
+    step1: function(req, res){
+        var index = Number(req.body.index);
+        var sql = `select * from v_a_money order by 발생일자 desc LIMIT ?, 5`;
+        var result = {state : true};
+        db("GET", sql, [index]).then(function(rows){
+            if(rows.state){
+                result.list = rows.rows;
+            } else {
+                result.list = [];
+                result.state = false;
+            }
+            m[8].step2(req, res, result); // step2 호출
+        });
+    },
+    step2: function(req, res, result){
+        sql = "select count(*) as cnt from v_a_money";
+        db("GET", sql, []).then(function(rows){     // 함수 실행 후 동작 부분
+            if(rows.state){
+                result.paging = rows.rows[0].cnt;
+            } else {
+                result.paging = 0;
+                result.state = false;
+            }
+            commons.msgResult(res, result, "msg0");
+        });
+    }
+}
+m[9] = {
+    path: "/insertaddbudget",
+    type: "POST",
+    fun: function(req, res){
+        var budgets_id = req.body.budgets_id;
+        var money = req.body.money;
+        var contents =  req.body.contents;
+        var flag= req.body.approval_flag;
+
+        var sql = 'insert into a_add_budgets (`budgets_id`, `money`,`contents`,`flag`) values (?, ?, ? ?)';
+        db("SET", sql, [budgets_id, money,contents,flag]).then(function(rows){
+            commons.msgResult(res, rows, "msg1", "../m3/index2.html#c1");//이동주소
+        });
+    }
+}
+
 /*
             res.set('Content-Type', 'text/html');
             res.set('charset',"utf-8");
